d4834c20be00514a1d90450090844cfd51da3d88
Andrew Bailey IV <bailey.andrew4@gmail.com>
2017-05-17 11:46:13 -0700

    check events works

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+"""
+This is a place for test scripts and utility functions to be placed
+"""
 ########################################################################
 # File: utils.py
 #  executable: utils.py
@@ -13,66 +16,84 @@
 from __future__ import print_function
 from timeit import default_timer as timer
 import sys
+import os
 import boto
 
 def find_skipped_events(filepath):
     """Find if there are any skipped events in a signalalign file or an event align file"""
+    # this is quite slow but it works
     set1 = set()
-    with open(filepath, 'r') as fh:
-        for line in fh:
-            # print(line.rstrip().split()[5])
+    with open(filepath, 'r') as file_handle:
+        for line in file_handle:
             set1.add(int(line.rstrip().split()[5]))
     return check_sequential(set1)
 
-
 def check_sequential(list_of_integers):
     """Make sure there are no gaps in a list of integers"""
-    l = list_of_integers
-    if sorted(l) == list(range(min(l), max(l)+1)):
-        return True
-    else:
-        return False
+    return bool(sorted(list_of_integers) == list(range(min(list_of_integers),\
+     max(list_of_integers)+1)))
 
-def grab_s3_files(bucket_path, ):
-    """Grab the paths to all fast5 files in a s3 bucket or in a local directory"""
-    bucket = bucket_path.split("/")
-    c = boto.connect_s3()
-    test = c.lookup(bucket[0])
+def grab_s3_files(bucket_path, ext=""):
+    """Grab the paths to files with an extention in a s3 bucket or in a local directory"""
+    # connect to s3
+    bucket_path = bucket_path.split("/")
+    conn = boto.connect_s3()
+    test = conn.lookup(bucket_path[0])
     if test is None:
         print("There is no bucket with this name!", file=sys.stderr)
         return 1
     else:
-        b = c.get_bucket(bucket[0])
+        bucket = conn.get_bucket(bucket_path[0])
     file_paths = []
-    for key in b.list("/".join(bucket[1:])):
-        if key.name[-5:] == "fast5":
-            file_paths.append(os.path.join("s3://", bucket[0], key.name))
+    # check file in each bucket
+    for key in bucket.list("/".join(bucket_path[1:])):
+        if ext == "":
+            file_paths.append(os.path.join("s3://", bucket_path[0], key.name))
+        else:
+            if key.name.split(".")[-1] == ext:
+                file_paths.append(os.path.join("s3://", bucket_path[0], key.name))
     return file_paths
 
-def list_dir(path, fast5=True):
+def list_dir(path, ext=""):
     """get all fast5 file paths from local directory"""
-    if fast5:
+    if ext == "":
         onlyfiles = [os.path.join(os.path.abspath(path), f) for f in \
-        os.listdir(bucket_path) if \
-        os.path.isfile(os.path.join(os.path.abspath(path), f)) \
-        if f[-5:] == "fast5"]
+        os.listdir(path) if \
+        os.path.isfile(os.path.join(os.path.abspath(path), f))]
     else:
-        onlyfiles = [os.path.join(os.path.abspath(path), f) for f in os.listdir(bucket_path)]
-    # print(onlyfiles)
+        onlyfiles = [os.path.join(os.path.abspath(path), f) for f in \
+        os.listdir(path) if \
+        os.path.isfile(os.path.join(os.path.abspath(path), f)) \
+        if f.split(".")[-1] == ext]
     return onlyfiles
 
+def check_events(directory):
+    """Check if all the tsv files from signal align match each event"""
+    counter = 0
+    good_files = []
+    # make sure each file has all events
+    for file1 in list_dir(directory, ext="tsv"):
+        if find_skipped_events(file1):
+            good_files.append(file1)
+        else:
+            counter += 1
+    # print how many failed and return files that passed
+    print("{} files had missing events".format(counter))
+    return good_files
 
 
 def main():
+    """Test the methods"""
     start = timer()
     file1 = "/Users/andrewbailey/nanopore-RNN/temp/tempFiles_alignment/132de6a8-df1e-468f-848b-abc960e1fc76_Basecall_2D_template.sm.backward.tsv"
     dir1 = "/Users/andrewbailey/nanopore-RNN/temp/tempFiles_alignment/"
-
-
-    print(find_skipped_events(file1))
+    print(len(grab_s3_files("bailey-nanonet/fast5files2", ext="a")))
+    # check_events(dir1)
+    print(len(list_dir(dir1, ext="a")))
+    # print(find_skipped_events(file1))
     stop = timer()
     print("Running Time = {} seconds".format(stop-start), file=sys.stderr)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
     raise SystemExit
