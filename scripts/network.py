aeeac432d0a1ef9d133557ea103b3f351e1c682f
Andrew Bailey IV <bailey.andrew4@gmail.com>
2017-05-24 12:17:17 -0700

    added logs folder for tensorflow and renamed nanoBLSTM to kerasBLSTM

--- scripts/network.py
+++ scripts/network.py
@@ -15,32 +15,82 @@
 
 from __future__ import print_function
 import sys
+import os
 from timeit import default_timer as timer
-import numpy as np
 from datetime import datetime
+import numpy as np
 from utils import project_folder
 import tensorflow as tf
 from tensorflow.contrib import rnn
 
 
+
 class BuildGraph():
     """Build a tensorflow network graph."""
-    def __init__(self, x):
-        self.x = x
+    #TODO make it possible to change the prediciton function, cost and optimizer
+    #   by creating functions for each of those
+    def __init__(self, n_steps, n_input, n_classes, learning_rate, n_layers=1,\
+    layer_sizes=tuple([128])):
+        self.x = tf.placeholder("float", [None, n_steps, n_input], name='x')
+        self.y = tf.placeholder("float", [None, n_classes], name='y')
+        global_step = tf.Variable(0, name='global_step', trainable=False)
 
-    def create_graph(self, num_layers, n_steps=1):
+        # Define weights and bias of last layer
+        weights = {
+            # Hidden layer weights => 2*n_hidden because of forward + backward cells
+            'out': tf.Variable(tf.random_normal([2*layer_sizes[-1], n_classes]))
+        }
+        biases = {
+            'out': tf.Variable(tf.random_normal([n_classes]))
+        }
+
+        outputs = self.create_graph(n_layers, layer_sizes=layer_sizes, n_steps=n_steps, )
+
+        # Linear activation, using rnn inner loop last output
+        with tf.name_scope("predition"):
+            pred = tf.matmul(outputs[-1], weights['out']) + biases['out']
+            self.variable_summaries(pred)
+
+        # Define loss and optimizer
+        with tf.name_scope("cost"):
+            self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred,\
+             labels=self.y))
+            self.variable_summaries(self.cost)
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.cost, \
+         global_step=global_step)
+
+        # Evaluate model
+        with tf.name_scope("correct_pred"):
+            correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(self.y, 1))
+            self.variable_summaries(self.cost)
+        with tf.name_scope("correct_pred"):
+            self.accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+            self.variable_summaries(self.accuracy)
+
+        self.merged_summaries = tf.summary.merge_all()
+        # Initializing the variables
+        self.init = tf.global_variables_initializer()
+
+
+
+    def create_graph(self, num_layers, layer_sizes=tuple([128]), n_steps=1):
         """Create a graph using a functions for different layers"""
+        # TODO create a graph with variable number of nodes in each blstm layer
         # Prepare data shape to match `bidirectional_rnn` function requirements
         # Current data input shape: (batch_size, n_steps, n_input)
         # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
         # Unstack to get a list of 'n_steps' tensors of shape (batch_size, n_input)
+        assert len(layer_sizes) == num_layers
         input1 = tf.unstack(self.x, n_steps, 1)
-        for number in range(1, num_layers+1):
-            input1 = self.blstm(input1, layer_name="layer"+str(number))
+        for number in range(num_layers):
+            input1 = self.blstm(input1, layer_name="layer"+str(number),\
+            n_hidden=layer_sizes[number])
         return input1
 
-    def blstm(self, input_vector, layer_name="layer1", n_hidden=128, forget_bias=5.0):
-        """Create a bidirectional LSTM using code from the example at https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/bidirectional_rnn.py"""
+    @staticmethod
+    def blstm(input_vector, layer_name="layer1", n_hidden=128, forget_bias=5.0):
+        """Create a bidirectional LSTM using code from the example at
+         https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/bidirectional_rnn.py"""
         # Define lstm cells with tensorflow
         # Forward direction cell
         lstm_fw_cell = rnn.LSTMCell(n_hidden, forget_bias=forget_bias)
@@ -56,19 +106,20 @@ class BuildGraph():
         # x, dtype=tf.float32)
         return outputs
 
-def variable_summaries(var):
-    """Attach a lot of summaries to a Tensor (for TensorBoard visualization).
-    source: https://github.com/tensorflow/tensorflow/blob/r1.1/tensorflow/examples/tutorials/mnist/mnist_with_summaries.py
-    """
-    with tf.name_scope('summaries'):
-        mean = tf.reduce_mean(var)
-        tf.summary.scalar('mean', mean)
-    with tf.name_scope('stddev'):
-        stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-    tf.summary.scalar('stddev', stddev)
-    tf.summary.scalar('max', tf.reduce_max(var))
-    tf.summary.scalar('min', tf.reduce_min(var))
-    tf.summary.histogram('histogram', var)
+    @staticmethod
+    def variable_summaries(var):
+        """Attach a lot of summaries to a Tensor (for TensorBoard visualization).
+        source: https://github.com/tensorflow/tensorflow/blob/r1.1/tensorflow/examples/tutorials/mnist/mnist_with_summaries.py
+        """
+        with tf.name_scope('summaries'):
+            mean = tf.reduce_mean(var)
+            tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
 
 
 def main():
@@ -87,7 +138,7 @@ def main():
     features2 = np.asarray([np.asarray(features[x]) for x in range(len(features))])
     labels2 = np.asarray([np.asarray(labels[x]) for x in range(len(labels))])
 
-
+    # TODO make hyperparameters a json file
     # Parameters
     learning_rate = 0.001
     training_iters = 10000
@@ -96,9 +147,9 @@ def main():
     # Network Parameters
     n_input = feature_len
     n_steps = 1 # one vector per timestep
-    n_hidden = 128 # hidden layer num of features
+    layer_sizes = tuple([100, 100, 100, 100]) # hidden layer num of features
     n_classes = label_len
-
+    n_layers = 4
 
     # TODO build factoring by batch size into a method
     # Should I make a data set class?
@@ -107,58 +158,24 @@ def main():
     batch1_y = labels2[:batch_size]
     batch1_x = batch1_x.reshape((batch_size, n_steps, n_input))
 
+    graph = BuildGraph(n_steps, n_input, n_classes, learning_rate, n_layers, layer_sizes)
+    x = graph.x
+    y = graph.y
+    cost = graph.cost
+    accuracy = graph.accuracy
+    merged_summaries = graph.merged_summaries
+    optimizer = graph.optimizer
 
-    # tf Graph input
-    x = tf.placeholder("float", [None, n_steps, n_input], name='x')
-    y = tf.placeholder("float", [None, n_classes], name='y')
-    global_step = tf.Variable(0, name='global_step', trainable=False)
-
-    # Define weights
-    weights = {
-        # Hidden layer weights => 2*n_hidden because of forward + backward cells
-        'out': tf.Variable(tf.random_normal([2*n_hidden, n_classes]))
-    }
-    biases = {
-        'out': tf.Variable(tf.random_normal([n_classes]))
-    }
-
-    graph = BuildGraph(x)
-    outputs = graph.create_graph(4)
-    # Linear activation, using rnn inner loop last output
-    with tf.name_scope("predition"):
-        pred = tf.matmul(outputs[-1], weights['out']) + biases['out']
-        variable_summaries(pred)
-
-    # Define loss and optimizer
-    with tf.name_scope("cost"):
-        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
-        variable_summaries(cost)
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost, \
-     global_step=global_step)
-
-    # Evaluate model
-    with tf.name_scope("correct_pred"):
-        correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-        variable_summaries(cost)
-    with tf.name_scope("correct_pred"):
-        accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-        variable_summaries(accuracy)
-
-    merged_summaries = tf.summary.merge_all()
-    # Initializing the variables
-    init = tf.global_variables_initializer()
-
+    init = graph.init
     # Launch the graph
     with tf.Session() as sess:
-        test_writer = tf.summary.FileWriter('/Users/andrewbailey/nanopore-RNN/logs'+\
-        datetime.now().strftime("%m%b-%d-%Hh-%Mm"), sess.graph)
+        # TODO make directory for each new run
+        logfolder_path = os.path.join(project_folder(), 'logs/', datetime.now().strftime("%m%b-%d-%Hh-%Mm"))
+        test_writer = tf.summary.FileWriter((logfolder_path), sess.graph)
         sess.run(init)
         step = 1
         # Keep training until reach max iterations
         while step * batch_size < training_iters:
-            # batch_x, batch_y = mnist.train.next_batch(batch_size)
-            # Reshape data to get 28 seq of 28 elements
-            # batch_x = batch_x.reshape((batch_size, n_steps, n_input))
             # Run optimization op (backprop)
             sess.run(optimizer, feed_dict={x: batch1_x, y: batch1_y})
             if step % display_step == 0:
